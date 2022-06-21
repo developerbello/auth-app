ca2af202cedee816b1a5a03ba49e024523f16098
belloibrahv <belloibrahv@gmail.com>
2022-06-21 16:30:33 -0700

    Use mui defined component to apply the form and button component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import UserRegister from "./component/UserRegister";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Register from "./components/Register";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import NoPage from "./pages/NoPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <UserRegister />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index path="/" element={<Register />} />
+          <Route path="/welcome" element={<Home />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
